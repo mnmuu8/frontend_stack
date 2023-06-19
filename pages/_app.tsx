0ba25d77c2bb3b4639d5b022e506097694a5f5e3
mnmuu8 <mnm.uu8@gmail.com>
2023-06-19 19:03:35 +0900

    Headerコンポーネント作成

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import Header from '@/components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { user } = pageProps;
-  
+
   return (
     <UserProvider>
+      <Header />
       <Component {...pageProps} />
     </UserProvider>
   );

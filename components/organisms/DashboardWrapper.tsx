66216f40e0e65ace6337f359a30e5d09fd42b63a
mnmuu8 <minami.yuya@valuence.inc>
2023-09-26 09:17:59 +0900

    APIでグラフデータ作成

---
+++ components/organisms/DashboardWrapper.tsx
@@ -0,0 +1,120 @@
+import React, { FC, useEffect, useState } from 'react'
+import Chart from '@/components/uikit/Chart'
+import RankTable from '@/components/uikit/RankTable'
+import { getSession } from '@/utiliry/session'
+import { StackProps } from '@/types/types'
+import axios from 'axios'
+
+const DashboardWrapper: FC = () => {
+  const [stacks, setStacks] = useState<StackProps[]>([]);
+  const [barData, setBarData] = useState<number[]>([]);
+  const [pieData, setPieData] = useState<number[]>([]);
+  const [pieLabels, setPieLabels] = useState<string[]>([]);
+
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1;
+  const daysInMonth = currentDate.getDate();
+
+  const generateLabels = (daysInMonth: number): string[] => {
+    const stackLabels: string[] = [];
+    for (let i = 1; i <= daysInMonth; i++) {
+      stackLabels.push(i.toString());
+    }
+    return stackLabels;
+  };
+
+  const barLabels = generateLabels(daysInMonth);
+
+  useEffect(() => {
+    const sessionData = getSession();
+    if (!sessionData) return;
+ 
+    const options = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${sessionData.token}`
+      },
+      params: {
+        user_id: sessionData.userId
+      }
+    }
+
+    axios.get('http://localhost:3000/api/v1/stacks', options)
+    .then(response => {
+      const { data } = response;
+      setStacks(data.stacks)
+
+      const aggregatedData = new Array(daysInMonth).fill(0);
+
+      data.stacks.forEach((stack: StackProps) => {
+        const stackDate = new Date(stack.stacked_at);
+        const stackYear = stackDate.getFullYear();
+        const stackMonth = stackDate.getMonth() + 1;
+        const stackDay = stackDate.getDate();
+        
+        const minutes = stack.minutes;
+        
+        if (stackYear === currentYear && stackMonth === currentMonth) {
+          aggregatedData[stackDay - 1] += minutes;
+        }
+      });
+      setBarData(aggregatedData)
+    })
+    .catch(error => {
+      if (error.response) {
+        const { data } = error.response;
+        throw new Error(`${JSON.stringify(data)}`);
+      } else {
+        throw new Error(`${JSON.stringify(error)}`);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!stacks.length) return;
+  
+    const skillMinutesMap: { [key: string]: number } = {};
+    stacks.forEach(stack => {
+      const skillName = stack.skill.name;
+      const minutes = stack.minutes;
+      skillMinutesMap[skillName] = (skillMinutesMap[skillName] || 0) + minutes;
+    });
+
+    const totalMinutes = Object.values(skillMinutesMap).reduce((acc, curr) => acc + curr, 0);
+    const skillLabels = Object.keys(skillMinutesMap);
+    const skillData = skillLabels.map(label => skillMinutesMap[label] / totalMinutes);
+
+    setPieData(skillData)
+    setPieLabels(skillLabels)
+  }, [stacks]);
+
+  return (
+    <div className='dashboard-page'>
+      <div className='text-2xl text-blue-900 font-bold mb-6'>積み上げ時間</div>
+      <div className='bg-white shadow-md p-6 w-full mb-10'>
+        <div className='w-8/12 m-auto flex justify-center'>
+          <Chart labels={barLabels} label={"時間"} data={barData} bdColor={["rgb(39, 119, 169)"]} bgColor={["rgb(240, 248, 250)"]} bdwidth={1} text={currentMonth + "月の学習時間"} type={"bar"} />
+        </div>
+      </div>
+      <div className='flex justify-between'>
+        <div className='w-[48%] flex flex-col'>
+          <div className='text-2xl text-blue-900 font-bold mb-6'>積み上げスキル</div>
+          <div className='bg-white shadow-md p-6 flex-grow'>
+            <div className='w-8/12 m-auto'>
+            <Chart labels={pieLabels} label={"学習時間"} data={pieData} bdColor={["rgb(39, 119, 169)"]} bgColor={["rgb(240, 248, 250)"]} bdwidth={1} text={"積み上げスキル"} type={"pie"} />
+            </div>
+          </div>
+        </div>
+        <div className='w-[48%] flex flex-col'>
+          <div className='text-2xl text-blue-900 font-bold mb-6'>積み上げランキング</div>
+          <div className='bg-white shadow-md p-6 flex-grow'>
+            <RankTable />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DashboardWrapper
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,13 @@
 import { NextPage } from 'next'
 import Layout from '@/components/organisms/Layout'
-import Chart from '@/components/uikit/Chart'
-import RankTable from '@/components/uikit/RankTable'
 import FormModal from '@/components/molecules/FormModal'
 import FormOpenButton from '@/components/atoms/FormOpenButton'
+import DashboardWrapper from '@/components/organisms/DashboardWrapper'
 
 const index: NextPage = () => {
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
-  const currentMonth = currentDate.getMonth() + 1;
-  const daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
-
-  // TODO: APIデータ受け取り。後々実装
-  const generateStudyTimeData = (daysInMonth: number): number[] => {
-    const studyTimeData: number[] = [];
-    for (let i = 0; i < daysInMonth; i++) {
-      studyTimeData.push(Math.floor(Math.random() * 16));
-    }
-    return studyTimeData;
-  }
-
-  const generateLabels = (daysInMonth: number): string[] => {
-    const stackLabels: string[] = [];
-    for (let i = 1; i <= daysInMonth; i++) {
-      stackLabels.push(i.toString());
-    }
-    return stackLabels;
-  };
-
-  const barData = generateStudyTimeData(daysInMonth);
-  const barLabels = generateLabels(daysInMonth);
-
-  const arcData = [30, 20, 40, 10, 5, 50];
-  const arcLabels = ["Ruby on Rails", "PHP", "React", "Next", "TypeScript", "WordPress"];
-  const arcBorderColor = ["#ffdad3", "#f9c5f7", "#b3f7bb", "#b4eaf7", "#faf49c", "#c9bbf7"];
-  const arcBackgroundColor = ["#ffdad3", "#f9c5f7", "#b3f7bb", "#b4eaf7", "#faf49c", "#c9bbf7"]
-
   return (
     <Layout>
-      <div className='dashboard-page'>
-        <div className='text-2xl text-blue-900 font-bold mb-6'>積み上げ時間</div>
-        <div className='bg-white shadow-md p-6 w-full mb-10'>
-          <div className='w-8/12 m-auto flex justify-center'>
-            <Chart labels={barLabels} label={"時間"} data={barData} bdColor={["rgb(39, 119, 169)"]} bgColor={["rgb(240, 248, 250)"]} bdwidth={1} text={currentMonth + "月の学習時間"} type={"bar"} />
-          </div>
-        </div>
-        <div className='flex justify-between'>
-          <div className='w-[48%] flex flex-col'>
-            <div className='text-2xl text-blue-900 font-bold mb-6'>積み上げスキル</div>
-            <div className='bg-white shadow-md p-6 flex-grow'>
-              <div className='w-8/12 m-auto'>
-              <Chart labels={arcLabels} label={"学習時間"} data={arcData} bdColor={arcBorderColor} bgColor={arcBackgroundColor} bdwidth={1} text={"積み上げスキル"} type={"pie"} />
-              </div>
-            </div>
-          </div>
-          <div className='w-[48%] flex flex-col'>
-            <div className='text-2xl text-blue-900 font-bold mb-6'>積み上げランキング</div>
-            <div className='bg-white shadow-md p-6 flex-grow'>
-              <RankTable />
-            </div>
-          </div>
-        </div>
-      </div>
+      <DashboardWrapper />
       <FormModal />
       <FormOpenButton formType={'createStack'} className={'fixed bottom-6 right-6 bg-blue-500 p-6 rounded-full hover:bg-blue-400 z-[100]'} />
     </Layout>

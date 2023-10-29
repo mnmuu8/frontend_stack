3d7122e6dd22de90e8bd2e033935f528c5773402
mnmuu8 <mnm.uu8@gmail.com>
2023-10-29 15:38:36 +0900

    templatesディレクトリ作成

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NextPage } from 'next'
 import Layout from '@/components/organisms/Layout'
-import MyPageWrapper from '@/components/organisms/MyPageWrapper'
+import MyPageWrapper from '@/components/templates/MyPageWrapper'
 import FormModal from '@/components/molecules/FormModal'
 
 const index: NextPage = () => {

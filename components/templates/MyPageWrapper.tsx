33e159a26bb2729116e6021bda95c72df2b4d045
ren0826nosuke <62938854+ren0826nosuke@users.noreply.github.com>
2023-12-21 17:35:32 +0900

    スキルランクのグラフに注釈とラベルを追加した (#83)
    * グラフ用のCSSを追加した
    
    * グラフの型定義を修正した”
    
    * スキルランクのラベルを追加した
    
    * スキルランクの内訳がhoverしたら見れるようにした

--- components/templates/MyPageWrapper.tsx
+++ components/templates/MyPageWrapper.tsx
@@ -1,5 +1,4 @@
 import React, { FC, useContext, useEffect, useState } from 'react';
-import { tabInfo } from '../../sample';
 import StackCard from '../molecules/StackCard';
 import ProfileCard from '../molecules/ProfileCard';
 import axios from 'axios';
@@ -120,33 +119,40 @@ const MyPageWrapper: FC = () => {
           />
         </div>
         <div className='py-6 flex items-center justify-center'>
-          <div className='flex items-center mr-4'>
+          <div className='hoverParent'>
             <span className='w-3 h-3 block bg-bronze mr-1'></span>
             <span className='text-sm'>ブロンズ</span>
+            <div className='hoverChild bg-bronze'>0~10,000(分)</div>
           </div>
-          <div className='flex items-center mr-4'>
+          <div className='hoverParent'>
             <span className='w-3 h-3 block bg-silver mr-1'></span>
             <span className='text-sm'>シルバー</span>
+            <div className='hoverChild bg-silver'>10,001~30,000(分)</div>
           </div>
-          <div className='flex items-center mr-4'>
+          <div className='hoverParent'>
             <span className='w-3 h-3 block bg-gold mr-1'></span>
             <span className='text-sm'>ゴールド</span>
+            <div className='hoverChild bg-gold'>30,001~60,000(分)</div>
           </div>
-          <div className='flex items-center mr-4'>
+          <div className='hoverParent'>
             <span className='w-3 h-3 block bg-platinum mr-1'></span>
             <span className='text-sm'>プラチナ</span>
+            <div className='hoverChild bg-platinum'>60,001~100,000(分)</div>
           </div>
-          <div className='flex items-center mr-4'>
+          <div className='hoverParent'>
             <span className='w-3 h-3 block bg-diamond mr-1'></span>
             <span className='text-sm'>ダイヤモンド</span>
+            <div className='hoverChild bg-diamond'>100,001~300,000(分)</div>
           </div>
-          <div className='flex items-center mr-4'>
+          <div className='hoverParent'>
             <span className='w-3 h-3 block bg-master mr-1'></span>
             <span className='text-sm'>マスター</span>
+            <div className='hoverChild bg-master'>300,001~600,000(分)</div>
           </div>
-          <div className='flex items-center mr-4'>
+          <div className='hoverParent'>
             <span className='w-3 h-3 block bg-legend mr-1'></span>
             <span className='text-sm'>レジェンド</span>
+            <div className='hoverChild bg-legend'>600,001~700,000(分)</div>
           </div>
         </div>
       </div>

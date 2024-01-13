ba474975cfb9f913e461caaec0c455b8026085a5
ren0826nosuke <ren0826jam@gmail.com>
2024-01-13 16:56:27 +0900

    TailWindClassを用いてスタイリングをした

--- pages/404.js
+++ pages/404.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import router from 'next/router';
 import Link from 'next/link';
-import styles from '../common/styles/Error.module.css';
 
 export default function Custom404() {
   return (
-    <div className={styles.container}>
+    <div className='errorContainer'>
       <h1 className='flex flex-col'>
-        <span className={`${styles.error} ${styles.errorCode}`}>404</span>
-        <span className={`${styles.error} ${styles.errorMessage}`}>Not Found</span>
+        <span className='errorBase errorCode'>404</span>
+        <span className='errorBase errorMessage'>Not Found</span>
       </h1>
       <p>ページが見つかりません</p>
       <p>お探しのページは存在しないか、移動した可能性があります。</p>
       <div className='flex'>
         <Link href="/" legacyBehavior>
-          <a className={styles.link}>ホームに戻る</a>
+          <a className='errorLink'>ホームに戻る</a>
         </Link>
-        <button onClick={() => router.back()} className={styles.link}>前のページに戻る</button>
+        <button onClick={() => router.back()} className='errorLink'>前のページに戻る</button>
       </div>
     </div>
   );
--- pages/500.js
+++ pages/500.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import router from 'next/router';
 import Link from 'next/link';
-import styles from '../common/styles/Error.module.css';
 
 export default function Custom500() {
   return (
-    <div className={styles.container}>
+    <div className='errorContainer'>
       <h1 className='flex flex-col'>
-        <span className={`${styles.error} ${styles.errorCode}`}>500</span>
-        <span className={`${styles.error} ${styles.errorMessage}`}>Internal Server Error</span>
+        <span className='errorBase errorCode'>500</span>
+        <span className='errorBase errorMessage'>Internal Server Error</span>
       </h1>
       <p>申し訳ありません。何らかの問題が発生しました。</p>
       <p>エラーが続く場合は、サイトの管理者に連絡して問題を報告してください。</p>
       <div className='flex'>
         <Link href="/" legacyBehavior>
-          <a className={styles.link}>ホームに戻る</a>
+        <a className='errorLink'>ホームに戻る</a>
         </Link>
-        <button onClick={() => router.back()} className={styles.link}>前のページに戻る</button>
+        <button onClick={() => router.back()} className='errorLink'>前のページに戻る</button>
       </div>
     </div>
   );

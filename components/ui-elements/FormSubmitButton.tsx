a9912cd72d5206646a13f68faa3055111c4982b7
mnmuu8 <mnm.uu8@gmail.com>
2024-01-19 15:19:56 +0900

    ファイルの文末に改行がないとエラーが発生するようにlintの設定変更

--- components/ui-elements/FormSubmitButton.tsx
+++ components/ui-elements/FormSubmitButton.tsx
@@ -4,11 +4,7 @@ import React, { FC } from 'react';
 
 const FormSubmitButton: FC<FormSubmitButtonProps> = ({ onClick, label }) => {
   return (
-    <Button
-      className='bg-blue-400 hover:bg-blue-300 text-white mx-2 w-full'
-      type='submit'
-      onClick={onClick}
-    >
+    <Button className='bg-blue-400 hover:bg-blue-300 text-white mx-2 w-full' type='submit' onClick={onClick}>
       {label}
     </Button>
   );

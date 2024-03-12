13f7bf6fec59b7ecc8e2d387eca53ce202be032c
ren0826nosuke <ren0826jam@gmail.com>
2024-03-12 23:51:44 +0900

    ユーザー更新用関数を別ファイルに切り出した
    import先ファイルを修正した

--- features/users/functions/update.ts
+++ features/users/functions/update.ts
@@ -1,25 +1,26 @@
 import axios from "axios";
 import { z } from 'zod';
 import { ErrorMessages } from "@/common/types/validator";
-import { CreateUserProps, UpdateUserProps } from "../types/api";
-import { userRegisterSchema, userSchema } from "@/common/functions/validator";
+import { UpdateUserProps } from "../types/api";
+import { userSchema } from "@/common/functions/validator";
 
-export const callCreateUser = async ({options, userFormData, setErrorMessages}: CreateUserProps) => {
+export const callUpdateUser = async ({options, sessionData, userFormData, setErrorMessages}: UpdateUserProps) => {
   try {
-    userRegisterSchema.parse(userFormData)
+    userSchema.parse(userFormData)
 
     const params = {
       role: userFormData.role,
       name: userFormData.name,
       email: userFormData.email,
       profile_content: userFormData.profile_content,
+      user_id: sessionData.userId,
       current_password: userFormData.current_password,
       new_password: userFormData.new_password,
       new_password_confirmation: userFormData.new_password_confirmation,
       team_id: userFormData.team.id,
     }
-    const url: string = `${process.env.API_ROOT_URL}/api/v1/users/`;
-    await axios.post(url, params, options);
+    const url: string = `${process.env.API_ROOT_URL}/api/v1/users/${sessionData.userId}`;
+    await axios.patch(url, params, options);
   } catch (error) {
     if (error instanceof z.ZodError) {
       const newErrors: ErrorMessages = {};
